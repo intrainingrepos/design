d9cb6ff90f59aec397f64641eaaa2ad83d3765a1
Siobhan Niess <35586990+niesssiobhan@users.noreply.github.com>
2019-01-07 23:55:39 -0800

    nav
    getting in the basic nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Header = props => {
   return (
     <header>
       <h1>{props.title}</h1>
       {props.children}
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
